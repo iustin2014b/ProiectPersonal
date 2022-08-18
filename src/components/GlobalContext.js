39be30764a4128d060f93351d90d99b5f81bc5a9
iustin2014b <107215989+iustin2014b@users.noreply.github.com>
2022-08-18 11:55:18 +0300

    Delete src directory

--- src/components/GlobalContext.js
+++
@@ -1,7 +0,0 @@
-import { useState, createContext, useContext } from "react";
-export const UserContext = createContext();
-export const setDatContext = createContext();
-export const globContext = createContext();
-export const genContext = createContext();
-
-
